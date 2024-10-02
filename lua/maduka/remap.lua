8868794eb68d5137f6c55d8ca04fdbab82d43d12
dilx <mdilshan21@gmail.com>
2024-10-02 18:21:40 +0530

    fix: update

--- lua/maduka/remap.lua
+++ lua/maduka/remap.lua
@@ -14,3 +14,6 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("v", "<leader>s", [[:<C-u>'<,'>s/]], { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>s", [[:%s/]], { noremap = true, silent = true })
