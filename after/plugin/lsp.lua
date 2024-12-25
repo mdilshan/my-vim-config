a72b867fbddae9bfce2309c7f3177791d2751123
dilx <mdilshan21@gmail.com>
2024-12-25 15:41:36 +0530

    sync

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,12 +17,13 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer'},
+  ensure_installed = {'tsserver', 'rust_analyzer', 'cypher_ls'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
       local lua_opts = lsp_zero.nvim_lua_ls()
       require('lspconfig').lua_ls.setup(lua_opts)
+      require('lspconfig')['cypher_ls'].setup {}  
     end,
   }
 })
