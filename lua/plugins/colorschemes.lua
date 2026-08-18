9ba482dbb74057e5f90d4804ef99d734da0e5ec9
dilx <mdilshan21@gmail.com>
2026-08-18 10:28:28 +0530

    fix: update

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,10 +1,26 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         require("tokyonight").setup({
+    --             style = "night",
+    --             transparent = true,
+    --         })
+    --         -- vim.cmd("colorscheme tokyonight")
+    --     end,
+    -- },
+    -- {
+    --     "rebelot/kanagawa.nvim",
+    --     name = "kanagawa",
+    --     config = function()
+    --         require("kanagawa").setup({
+    --             transparent = true,
+    --         })
+    --         vim.cmd("colorscheme kanagawa")
+    --     end,
+    -- }
     {
         "rose-pine/neovim",
         name = "rose-pine",
@@ -18,4 +34,3 @@ return {
         end,
     },
 }
-
